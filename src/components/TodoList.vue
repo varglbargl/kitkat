44d89ac165519b5f05f38218cd6f9c0ecd5f4e42
Vanessa <vbargodale@gmail.com>
2021-05-04 17:59:05 -0700

    folder structure in place to start work from laptop

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <div id="list">
+    <h3 v-for="(todo, index) in todos" :key="index">{{ todo }}</h3>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      todos: [
+        'dental plan',
+        'lisa needs braces',
+        'home lawn',
+        'refinancin',
+        'i dont know'
+      ]
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 20px 0 0;
+}
+
+</style>
